2b8006254af114edb7596f06763b7ae41d66638e
Mel Belachew <melbelachew@gmail.com>
2019-10-09 16:04:45 -0400

    first-commit-303

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -37,5 +34,23 @@ public class HomeController {
         todoRepository.save(todo);
         return "redirect:/";
 
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showTodo(@PathVariable("id") long id, Model model){
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateTodo(@PathVariable("id") long id, Model model){
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "todoform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteTodo(@PathVariable("id") long id){
+        todoRepository.deleteById(id);
+        return "redirect:/";
     }
 }
